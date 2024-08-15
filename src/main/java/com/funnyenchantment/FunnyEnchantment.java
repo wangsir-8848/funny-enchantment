980c69af44246718f557dc543ab4d5e79ec14f57
wangguoqing <wangsir777@yeah.net>
2024-08-15 11:30:53 +0800

    init

---
+++ src/main/java/com/funnyenchantment/FunnyEnchantment.java
@@ -0,0 +1,19 @@
+package com.funnyenchantment;
+
+import net.fabricmc.api.ModInitializer;
+
+/**
+ * @author wanggq
+ * @version 1.0.0
+ * @date 2024/8/15
+ * @description TDDD
+ */
+public class FunnyEnchantment implements ModInitializer {
+    /**
+     * Runs the mod initializer.
+     */
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/com/funnyenchantment/client/FunnyEnchantmentClient.java
@@ -0,0 +1,19 @@
+package com.funnyenchantment.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+/**
+ * @author wanggq
+ * @version 1.0.0
+ * @date 2024/8/15
+ * @description TDDD
+ */
+public class FunnyEnchantmentClient implements ClientModInitializer {
+    /**
+     * Runs the mod initializer on the client environment.
+     */
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
