caa828adf69e50b1afc38c7cfe2a4b528a89e99a
wangguoqing <wangsir777@yeah.net>
2024-08-16 16:40:40 +0800

    新增两本附魔数

---
+++ src/main/java/com/funnyenchantment/enchantment/VampirismEnchantment.java
@@ -0,0 +1,32 @@
+package com.funnyenchantment.enchantment;
+
+import net.minecraft.enchantment.Enchantment;
+import net.minecraft.enchantment.EnchantmentTarget;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.entity.LivingEntity;
+
+/**
+ * @author wanggq
+ * @version 1.0.0
+ * @date 2024/8/16
+ * @description 吸血书
+ */
+public class VampirismEnchantment extends Enchantment {
+    public VampirismEnchantment(Rarity rarity, EquipmentSlot... slotTypes) {
+        super(rarity, EnchantmentTarget.WEAPON, slotTypes);
+    }
+    @Override
+    public int getMaxLevel() {
+        return 3;
+    }
+
+    @Override
+    public void onTargetDamaged(LivingEntity user, Entity target, int level) {
+        // 20%的几率 吸血
+        if (user.getRandom().nextInt(5) == 1){
+            user.heal((float) (0.5*level));
+        }
+        super.onTargetDamaged(user, target, level);
+    }
+}
